65910ee181fd9a982854dd32f5ff109c1aadfd44
Kristina <kristina@pop-os.localdomain>
2020-11-25 13:24:17 +0200

    client: fixed footer

--- client/src/components/footer/FooterStyle.jsx
+++ client/src/components/footer/FooterStyle.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 const FooterStyle = styled.div`
-  margin-top: 20px;
-  display: flex;
-  justify-content: center;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color: hsla(315, 100%, 36%, 1);
+  text-align: center;
   background: lightpink;
-  margin-bottom: -10px;
+  padding: 20px;
+  font-size: 24px;
+  justify-content: center;
 `;
 
 export default FooterStyle;
--- client/src/components/nav-bar/NavLink.jsx
+++ client/src/components/nav-bar/NavLink.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavLinkStyled from "./NavLinkStyled";
 import Anchor from "components/miscellaneous/Anchor";
-import Button from "components/miscellaneous/Button";
 
 const NavLink = ({ title, to }) => (
   <NavLinkStyled>
